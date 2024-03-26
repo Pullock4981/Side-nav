e4006db239cc1303b89948f46f6ca9f567fd25aa
Pullock4981 <pullock15-4981@diu.edu.bd>
2024-03-26 11:54:05 +0600

    add main section

---
+++ sidenav.js
@@ -0,0 +1,16 @@
+// const folders = document.querySelectorAll('.folder');
+// for(folder of folders){
+//     folder.onClick = ()=>{
+//         console.log(folder.nextElementSibling)
+//     }
+// }
+// // console.log(folders)
+
+// // for (const folder of folders) {
+// //     folder.onClick = () => {
+// //         console.log(folder.nextElementSibling)
+// //     }
+// // }
+
+// // console.log('hi')
+
